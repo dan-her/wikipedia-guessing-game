ba04bb9fb0b06ab31195990fdb2d5eabee29c8d5
dan-her <danhercik911@gmail.com>
2022-05-01 18:48:32 -0400

    added python code and refactored html to not work at all.            to actually get the python code to run, you need to run it and then go to localhost:8000 in a browser

---
+++ crawler.py
@@ -0,0 +1,43 @@
+import requests
+import json
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/', methods = ['POST'])
+def index():
+    with open('data.json', 'w') as fil:
+        output = crawl()
+        json.dump(output, fil)
+
+def crawl():
+    finished = 0;
+    while (finished == 0):
+        seed_url = 'https://en.wikipedia.org/wiki/Special:Random'
+        page = requests.get(seed_url)
+        body = page.text
+    
+        titleWF = body[body.find('<h1'):body.find('</h1')]
+        realTitle = titleWF[titleWF.find('>'):]
+        realTitle = realTitle[1:]
+        if (realTitle[:3] == '<i>'):
+            realTitle = realTitle[3:realTitle.find('<')]
+        
+        startOB = '<b>' + realTitle
+        question = body[body.find(startOB):body.find('</p>')]
+        question = question[3:question.find('</p>')]
+        while (question.find('<') != -1):
+            start = question.find('<')
+            end = question.find('>')
+            question = question.replace(question[start:(end+1)], '')
+        if (question == ''):
+            continue
+        question = question.replace(realTitle, '__________')
+        if (question.count('_') > 10):
+            continue
+        finished = 1
+        print(realTitle)
+        print(question)
+    return {'answer':realTitle, 'quest':question}
+    
+if __name__ == "__main__":
+    app.run(port = 8000)
